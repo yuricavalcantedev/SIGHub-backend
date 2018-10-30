cb896c793a75f78b5c8e6a763153d760d90c4bd8
yuricavalcantedev <yuricavalcante17@gmail.com>
2018-10-30 00:47:53 -0300

    Exceptions classes and some services classes were added

--- TccTool/src/main/java/br/com/yuri/cavalcante/tcc/repositories/ApplicationDomainRepository.java
+++ TccTool/src/main/java/br/com/yuri/cavalcante/tcc/repositories/ApplicationDomainRepository.java
@@ -3,7 +3,9 @@ package br.com.yuri.cavalcante.tcc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.yuri.cavalcante.tcc.domain.ApplicationDomain;
+
 @Repository
-public interface ApplicationDomainRepository extends JpaRepository<ApplicationDomainRepository, Integer>{
+public interface ApplicationDomainRepository extends JpaRepository<ApplicationDomain, Integer>{
 
 }
--- TccTool/src/main/java/br/com/yuri/cavalcante/tcc/repositories/AreaRepository.java
+++ TccTool/src/main/java/br/com/yuri/cavalcante/tcc/repositories/AreaRepository.java
@@ -3,7 +3,9 @@ package br.com.yuri.cavalcante.tcc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.yuri.cavalcante.tcc.domain.Area;
+
 @Repository
-public interface AreaRepository extends JpaRepository<AreaRepository, Integer>{
+public interface AreaRepository extends JpaRepository<Area, Integer>{
 
 }
